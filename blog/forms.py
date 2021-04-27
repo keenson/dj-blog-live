30e7831e5900003f9efb8327a42716a86b85c1d0
keenson <keensono@gmail.com>
2021-04-27 22:08:12 +0100

    Updated settings

---
+++ blog/forms.py
@@ -0,0 +1,29 @@
+from .models import Post, Comment
+from django import forms
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'author', 'body')
+        
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type':'hidden'}), 
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'body' )
+        
+        widgets = {
+            'name' : forms.TextInput(attrs={'class': 'form-control'}),
+            'body' : forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
---
+++ blog/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from .models import Post, Comment
+from .forms import CommentForm, PostForm
+
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post    
+    form_class = PostForm
+    template_name = 'post_new.html'
+    # fields = ['title', 'author' ,'body']
+    
+class BlogCommentView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'add_comment.html'
+    # fields = '__all__'
+    def form_valid(self,form):
+        form.instance.post_id = self.kwargs['pk']
+        return super().form_valid(form)
+    
+    success_url = reverse_lazy('home')
+    
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+    
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
