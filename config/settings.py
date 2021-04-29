2e804bd79810db431e596298c981738b23b6409d
keenson <keensono@gmail.com>
2021-04-29 17:15:01 +0100

    added allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['alagogo.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
